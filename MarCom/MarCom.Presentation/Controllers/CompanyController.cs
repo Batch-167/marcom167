aace41767262f4237a96b8497a05a317ec28760d
bayupradinta19 <bayupradinta19@gmail.com>
2018-10-01 11:06:17 +0700

    Master Company

--- MarCom/MarCom.Presentation/Controllers/CompanyController.cs
+++ MarCom/MarCom.Presentation/Controllers/CompanyController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MarCom.Repository;
+using MarCom.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +15,84 @@ namespace MarCom.Presentation.Controllers
         {
             return View();
         }
+
+        public ActionResult List()
+        {
+            return PartialView("_List", CompanyRepo.Get());
+        }
+
+        //GET
+        public ActionResult Create()
+        {
+            return PartialView("_Create", new CompanyViewModel());
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Create(CompanyViewModel model)
+        {
+            if (CompanyRepo.Update(model))
+            {
+                return Json(new { success = true, entity = model }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        //GET
+
+        public ActionResult Edit(int id)
+        {
+            CompanyViewModel model = CompanyRepo.GetById(id);
+            return PartialView("_Edit", model);
+        }
+
+        //POST
+        [HttpPost]
+        public ActionResult Edit(CompanyViewModel model)
+        {
+            if (CompanyRepo.Update(model))
+            {
+                return Json(new { success = true, entity = model }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            CompanyViewModel model = CompanyRepo.GetById(id);
+            return PartialView("_Delete", model);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(CompanyViewModel model)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteConfirm(int id)
+        {
+            if(CompanyRepo.Delete(id))
+            {
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            CompanyViewModel model = CompanyRepo.GetById(id);
+            return PartialView("_Details", model);
+        }
+
     }
 }
---
+++ MarCom/MarCom.ViewModel/CompanyViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarCom.ViewModel
+{
+    public class CompanyViewModel
+    {
+        public CompanyViewModel()
+        {
+            Is_Delete = false;
+           }
+
+        [DisplayName("No")]
+        public int Row { get; set; }
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Company Code")]
+        public string Code { get; set; }
+
+        [DisplayName("Company Name")]
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        public bool Is_Delete { get; set; }
+
+        [DisplayName("Create By")]
+        public string Create_By { get; set; }
+
+        [DisplayName("Create Date")]
+        public DateTime Create_Date { get; set; }
+
+    }
+}
